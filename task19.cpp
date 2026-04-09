b9640d0c601bc1521bff6c0f954e11539f7fe7ec
Arsen Yatsuta <yatsuta.a.s.-tv52@edu.kpi.ua>
2026-04-09 18:16:20 +0300

    День 2: підказки, меню, safeReadInt, виправлено розміщення rowHints всередині циклу

--- task19.cpp
+++ task19.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 //День 1: визначились з розмірами поля
@@ -10,14 +11,37 @@ const int COLS = 6;
 // День 1: вирішили зберігати поле в динамічній пам'яті (згідно з вимогою курсової роботи)
 bool* active;
 
-// День1: прототип, будемо одразу дотримуватись такої структури коду
+// День 2: додаємо підказки (те, які цифри мають зустрічатись у кожному рядку або колонці)
+vector<int>* rowHints;
+vector<int>* colHints;
+
+// День 2: прототипи
 void initData();
 void cleanupData();
+void printMenu();
 void printField();
+int safeReadInt(int* minVal, int* maxVal);
 
 int main() {
     initData();
-    printField();
+
+    int choice = 0;
+    int* ptrChoice = &choice;
+    do {
+        printMenu();
+        int minV = 0, maxV = 2;
+        // День 2: поки лише 2 пункти меню: показати поле та вийти
+        // TODO: потім додати автоматичний розв'язок та режим гравця 
+        *ptrChoice = safeReadInt(&minV, &maxV);
+        switch (*ptrChoice) {
+            case 1: printField(); break;
+            case 0: {
+                cout << "\n До побачення! \n";
+                break;
+            }
+        }
+    } while (*ptrChoice != 0);
+    
     cleanupData();
     return 0;
 }
@@ -38,30 +62,104 @@ void initData() {
 
     for (int i = 0; i < ROWS; i++)
       for (int j = 0; j < COLS; j++)
-        *(active + i * COLS + j) =
-initActive[i][j];
+        *(active + i * COLS + j) = initActive[i][j];
+
+    // День 2: ініціалізація підказок
+    rowHints = new vector<int>[ROWS];
+    colHints = new vector<int>[COLS];
+
+    // Заповнюємо лише ті рядки, що мають підказки
+    *(rowHints + 0) = {2, 0, 1, 5};
+    *(rowHints + 3) = {1, 3, 4};
+    *(rowHints + 6) = {0, 2, 3};
+
+    *(colHints + 0) = {2, 4, 6};
+    *(colHints + 1) = {0, 1, 5};
+    *(colHints + 2) = {0, 3, 6};
+    *(colHints + 3) = {1, 3, 6};
+    *(colHints + 4) = {0, 1, 5};
+    *(colHints + 5) = {1, 2, 3, 4};
 }
 
 void cleanupData() {
     delete[] active;
+    // День 2: чистимо нову пам'ять
+    delete[] rowHints;
+    delete[] colHints;
 }
 
-// День 1: найпростіше відображення: #, де активна клітинка
-// TODO: потім додати підказки рядків і колонок
-// TODO: потім додати відображення значень кісточок
+// День 2: базове меню без рамок (їх зробимо пізніше)
+void printMenu() {
+    cout << "\n === ГОЛОВОЛОМКА ДОМІНО ===\n";
+    cout << " [1] Показати поле\n";
+    cout << " [0] Вийти\n";
+    cout << " Ваш вибір\n";
+}
+
+// День 2: безпечне читання числа з перевіркою діапазону
+int safeReadInt(int* minVal, int* maxVal) {
+    int val;
+    string line;
+    while (true) {
+        if (getline(cin, line)) {
+            bool valid = !line.empty();
+            for (int i = 0; i < (int)line.length(); i++) {
+                if (line[i] < '0' || line[i] > '9') { valid = false; break; }
+            }
+            if (valid) {
+                try {
+                    val = stoi(line);
+                    if (val >= *minVal && val <= *maxVal) return val;
+                    cout << "  Число від " << minVal << " до " << *maxVal << ": ";
+                } catch (...) {
+                    cout << " Помилка! Введіть ціле число: ";
+                }
+            } else {
+                cout << " Помилка! Введіть ціле число: ";
+            }
+        }
+    }
+}
+
+// День 2: оновлений printField (додали підказки рядків та колонок)
 
 void printField() {
-    cout << "\n";
+    cout << "\n  +";
+    for (int j = 0; j < COLS; j++) cout << "---+";
+    cout <<"\n";
 
     for (int i = 0; i < ROWS; i ++) {
         cout << " |";
         for (int j = 0; j < COLS; j++) {
-            // День 1: поки просто #, числа з'являться пізніше
-            if (*(active + i * COLS +j)) cout << "#|";
+            if (*(active + i * COLS +j)) cout << " # |";
             else cout << " |";
         }
         cout << "\n +";
         for (int j = 0; j < COLS; j++) cout << "---+";
         cout << "\n";
+        
+    // День 2: виводимо підказки рядка праворуч
+    if (!(*(rowHints + i)).empty()) {
+        cout << " <";
+        for (int h : *(rowHints + i)) cout << " " << h;
     }
+    cout << "\n +";
+    for (int j = 0; j < COLS; j++) cout << "---+";
+    cout << "\n";
+
+    // День 2: виводимо підказки колонок внизу
+    cout << "    ";
+    for (int j = 0; j < COLS; j++) cout << "  ^ ";
+    cout << "\n";
+    for (int line = 0; line < 4; line++) {
+        cout << "    ";
+        for (int j = 0; j < COLS; j++) {
+            if (line < (int)(*(colHints + j)).size())
+            cout << "  " << (*(colHints + j))[line] << " ";
+        else 
+            cout << "    ";
+        }
+        cout << "\n";
+    }
+
 }
